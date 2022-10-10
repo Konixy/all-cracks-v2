6e16ab8af35e290f2f3c894fd53be5dd5b9a94cc
Anatole Dufour <konixy.p@gmail.com>
2022-10-10 19:42:19 +0200

    Initial commit

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./index.scss";
+import Home from './Home'
+import Header from "./Header";
 
 class App extends React.Component {
   render() {
     return (
-    <div>My App</div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="*" element={<>404</>} />
+        </Routes>
+      </Router>
     )
   }
 }
