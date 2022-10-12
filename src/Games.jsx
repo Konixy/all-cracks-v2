20a0d7acd56c784e6c1a50fdf05ad8e005da9eec
Konixy <66780340+Konixy@users.noreply.github.com>
2022-10-13 00:05:49 +0200

    Update

--- src/Games.jsx
+++ src/Games.jsx
@@ -1,9 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class Games extends Component {
+  constructor() {
+    super();
+    this.state = {
+      games: [],
+    };
+  }
   render() {
     return (
-      <div>Games</div>
-    )
+      <main>
+        <div className="container">
+          {this.state.games.length > 0 ? (
+            this.state.games.map(e => (
+              <div className="game" key={e.name}><Link to={e.href} className="text-md">e.name</Link></div>
+            ))
+          ) : (
+            <>{"Games are loading"}</>
+          )}
+        </div>
+      </main>
+    );
   }
 }
