592b0e5036e085fb0a48d1c468cdafbb12e7a314
Konixy <konixy.p@gmail.com>
2022-10-29 22:34:12 +0200

    Update

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,7 +6,7 @@ export default class Home extends Component {
   render() {
     return (
       <div className="container items-center text-center my-20 mx-10 w-full">
-        <div className="w-fit m-0">
+        <div className="w-full m-0">
           <h1 className="text-2xl text-center lg:mx-20">Touts vos jeux préférés, gratuits et téléchargable en un seul <span className="text-blue-500 underline">click</span><i className="fa-solid fa-arrow-pointer absolute translate-y-5 -translate-x-3 text-black text-shadow-white"></i> !</h1>
           <div className="relative mt-10 flex flex-row justify-center items-center">
             <PrimaryButton href="/games" type="link" className="mr-4">
