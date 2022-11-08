a6f01427285c5792fe5bb7d10545bd450b0b6e11
Anatole Dufour <konixy.p@gmail.com>
2022-11-08 21:35:21 +0100

    Update

--- src/Header.tsx
+++ src/Header.tsx
@@ -108,25 +108,13 @@ export default class Header extends Component {
           <div className="w-[108rem] flex-none flex justify-end">
             <picture>
               <source
-                srcSet="https://tailwindcss.com/_next/static/media/docs@30.8b9a76a2.avif"
-                type="image/avif"
-              />
-              <img
-                src="https://tailwindcss.com/_next/static/media/docs@tinypng.d9e4dcdc.png"
-                alt=""
-                className="w-[71.75rem] flex-none max-w-none dark:hidden"
-                decoding="async"
-              />
-            </picture>
-            <picture>
-              <source
                 srcSet="https://tailwindcss.com/_next/static/media/docs-dark@30.1a9f8cbf.avif"
                 type="image/avif"
               />
               <img
                 src="https://tailwindcss.com/_next/static/media/docs-dark@tinypng.1bbe175e.png"
                 alt=""
-                className="w-[90rem] flex-none max-w-none hidden dark:block"
+                className="w-[90rem] flex-none max-w-none block"
                 decoding="async"
               />
             </picture>
